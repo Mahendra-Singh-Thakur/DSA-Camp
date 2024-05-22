5170a5fef37ad50a7e4457ecead85b23d9746aeb
Mahendra-Singh-Thakur <thakurmahendrasingh907@email.com>
2024-05-22 16:36:00 +0530

    stack application complete

--- DSA/SubTopics/Stack/Applications/app.js
+++ DSA/SubTopics/Stack/Applications/app.js
@@ -1,2 +1,13 @@
-const first=localStorage.getItem('lastClicked');
-console.log(first);
+let contentshow = document.querySelectorAll('.cont');
+
+contentshow.forEach((c) => {
+    c.addEventListener('click', (e) => {
+        e.preventDefault();
+        let paragraph = c.querySelector('p'); 
+        if (paragraph.classList.contains('hide')) {
+            paragraph.classList.remove('hide'); 
+        } else {
+            paragraph.classList.add('hide'); 
+        }
+    })
+})
